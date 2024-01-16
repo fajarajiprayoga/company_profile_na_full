bc8b98d2a33ba13691b7a43ffd0e87eb46097794
fajarajiprayoga <fajaraji.prayoga@gmail.com>
2024-01-16 10:35:55 +0700

    OTP Email mekararmadajaya and create footer seeder

--- app/Livewire/Login.php
+++ app/Livewire/Login.php
@@ -12,6 +12,9 @@ use Illuminate\Support\Facades\Hash;
 use Usernotnull\Toast\Concerns\WireToast;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+use PHPMailer\PHPMailer\SMTP;
 
 
 class Login extends Component
@@ -22,6 +25,8 @@ class Login extends Component
 
     public function login(Request $request)
     {
+        require base_path("vendor/autoload.php");
+
         $this->loginForm->validate(); 
         
         $checkAuth = Auth::attempt([
@@ -39,9 +44,68 @@ class Login extends Component
             $user->otp_expired = $exp_otp_code;
             $user->update();
 
-            Mail::to($user->email)->send(new OtpMail($otp_code, $user->name));
+            // Mail::to($user->email)->send(new OtpMail($otp_code, $user->name));
 
-            $this->redirectRoute('otp', ['email' => $this->loginForm->email]);
+            $mail = new PHPMailer();
+				$mail->SMTPDebug = SMTP::DEBUG_SERVER;
+				// $mail->isSMTP();
+				// $mail->Host = 'sandbox.smtp.mailtrap.io';
+				// $mail->SMTPAuth = true;
+				// $mail->Port = 2525;
+				// $mail->Username = '897bc8d1cb3468';
+				// $mail->Password = '1cf6e3a2fd59b7';
+
+				$mail->IsSMTP();
+				$mail->SMTPOptions = array(
+						'ssl' => array(
+						'verify_peer' => false,
+						'verify_peer_name' => false,
+						'allow_self_signed' => true
+						)
+					);
+				$mail->Host = 'ssl://mail.mekararmadajaya.com';
+				$mail->SMTPAuth = true;
+				$mail->SMTPSecure = 'ssl';
+				$mail->SMTPAutoTLS = false;
+				$mail->Port = 465;
+				$mail->Username = 'official1_admin@mekararmadajaya.com';
+				$mail->Password = 'adm@RM4D4';
+
+				//Recipients
+				$senderEmail = 'official1_admin@mekararmadajaya.com';
+				$senderName = 'Company Profile New Armada';
+				$mail->setFrom($senderEmail, $senderName);
+				$mail->addAddress($user->email, $user->name);
+
+				//Content
+				$mail->isHTML(true);
+				$mail->Subject = 'Kode OTP E-Katalog';
+				$mail->Body = "
+					<div>
+						<h3>Kode Verifikasi Login Company Profile New Armada</h3>
+					</div>
+					<div>
+						<span>Halo $user[name],</span>
+					</div>
+					<div style='padding-top: 10px; padding-bottom: 10px;'>
+						<span>Silahkan masukkan kode OTP ini untuk dapat melanjutkan menggunakan aplikasi New Armada.</span>
+					</div>
+					<div style='background-color: #e3e3e3; padding: 15px;'>
+						<span>OTP</span>
+						<h4>$otp_code</h4>
+						<span>Kode ini berlaku selama 10 menit.<span>
+					</div>
+					<div style='padding-top: 10px; padding-bottom: 10px;'>
+						<span>Jaga keamanan akun Anda dengan tidak membagikan kode OTP kepada siapapun.</span>
+					</div>
+					<span>Terimakasih.</span>
+				";
+				$mail->AltBody = 'OTP Company Profile New Armada = ' . $otp_code;
+                if(!$mail->send()){
+                    return redirect()->route('login')->with('failed', 'Login failed, pleas try again');        
+                }else{
+                    $this->redirectRoute('otp', ['email' => $this->loginForm->email]);
+                }            
 
         }else {
             return redirect()->route('login')->with('failed', 'Login failed, pleas try again');
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,6 +6,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use Spatie\Permission\Models\Role;
 use Spatie\Permission\Models\Permission;
+use App\Models\Footer;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,10 +19,25 @@ class DatabaseSeeder extends Seeder
 
         $user = \App\Models\User::factory()->create([
             'name' => 'admin',
-            'email' => 'admin@gmail.com',
+            'email' => 'fajar.prayoga@students.amikom.ac.id',
         ]);
 
         $role = Role::create(['name' => 'super-admin']);
         $user->assignRole($role);
+
+        $footer = new Footer;
+        $footer->name = "Global Setting";
+        $footer->address = "Address";
+        $footer->youtube_url = "https://youtube.com";
+        $footer->instagram_url = "https://instagram.com";
+        $footer->instagram_username = "@username";
+        $footer->facebook_url = "https://facebook.com";
+        $footer->shopee_url = "https://shopee.com";
+        $footer->tokopedia_url = "https://tokopedia.com";
+        $footer->email = "email@gmail.com";
+        $footer->background_product = "https://media.istockphoto.com/id/1149329096/id/foto/balon-udara-panas-di-atas-matahari-terbenam-gunung-moses-sinai.jpg?s=2048x2048&w=is&k=20&c=5TCuKMX-D_AgNPTCfYQMPpMD746wEJQ25AWWrqCc_kI=";
+        $footer->background_contact = "https://media.istockphoto.com/id/1149329096/id/foto/balon-udara-panas-di-atas-matahari-terbenam-gunung-moses-sinai.jpg?s=2048x2048&w=is&k=20&c=5TCuKMX-D_AgNPTCfYQMPpMD746wEJQ25AWWrqCc_kI=";
+        $footer->background_download_center = "https://media.istockphoto.com/id/1149329096/id/foto/balon-udara-panas-di-atas-matahari-terbenam-gunung-moses-sinai.jpg?s=2048x2048&w=is&k=20&c=5TCuKMX-D_AgNPTCfYQMPpMD746wEJQ25AWWrqCc_kI=";
+        $footer->save();
     }
 }
