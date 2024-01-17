229d0b409dc5b448295cae983f02b9622a9f8877
Fajar Aji Prayoga <fajaraji.prayoga@gmail.com>
2024-01-17 07:29:32 +0700

    Update send email OTP

--- app/Livewire/Login.php
+++ app/Livewire/Login.php
@@ -44,69 +44,69 @@ class Login extends Component
             $user->otp_expired = $exp_otp_code;
             $user->update();
 
-            Mail::to($user->email)->send(new OtpMail($otp_code, $user->name));
+            // Mail::to($user->email)->send(new OtpMail($otp_code, $user->name));
 
-            // $mail = new PHPMailer();
-			// 	$mail->SMTPDebug = SMTP::DEBUG_SERVER;
-			// 	// $mail->isSMTP();
-			// 	// $mail->Host = 'sandbox.smtp.mailtrap.io';
-			// 	// $mail->SMTPAuth = true;
-			// 	// $mail->Port = 2525;
-			// 	// $mail->Username = '897bc8d1cb3468';
-			// 	// $mail->Password = '1cf6e3a2fd59b7';
+            $mail = new PHPMailer();
+				// $mail->SMTPDebug = SMTP::DEBUG_SERVER;
+				// $mail->isSMTP();
+				// $mail->Host = 'sandbox.smtp.mailtrap.io';
+				// $mail->SMTPAuth = true;
+				// $mail->Port = 2525;
+				// $mail->Username = '897bc8d1cb3468';
+				// $mail->Password = '1cf6e3a2fd59b7';
 
-			// 	$mail->IsSMTP();
-			// 	$mail->SMTPOptions = array(
-			// 			'ssl' => array(
-			// 			'verify_peer' => false,
-			// 			'verify_peer_name' => false,
-			// 			'allow_self_signed' => true
-			// 			)
-			// 		);
-			// 	$mail->Host = 'ssl://mail.mekararmadajaya.com';
-			// 	$mail->SMTPAuth = true;
-			// 	$mail->SMTPSecure = 'ssl';
-			// 	$mail->SMTPAutoTLS = false;
-			// 	$mail->Port = 465;
-			// 	$mail->Username = 'official1_admin@mekararmadajaya.com';
-			// 	$mail->Password = 'adm@RM4D4';
+				$mail->IsSMTP();
+				$mail->SMTPOptions = array(
+						'ssl' => array(
+						'verify_peer' => false,
+						'verify_peer_name' => false,
+						'allow_self_signed' => true
+						)
+					);
+				$mail->Host = 'ssl://mail.mekararmadajaya.com';
+				$mail->SMTPAuth = true;
+				$mail->SMTPSecure = 'ssl';
+				$mail->SMTPAutoTLS = false;
+				$mail->Port = 465;
+				$mail->Username = 'official1_admin@mekararmadajaya.com';
+				$mail->Password = 'adm@RM4D4';
 
-			// 	//Recipients
-			// 	$senderEmail = 'official1_admin@mekararmadajaya.com';
-			// 	$senderName = 'Company Profile New Armada';
-			// 	$mail->setFrom($senderEmail, $senderName);
-			// 	$mail->addAddress($user->email, $user->name);
+				//Recipients
+				$senderEmail = 'official1_admin@mekararmadajaya.com';
+				$senderName = 'Company Profile New Armada';
+				$mail->setFrom($senderEmail, $senderName);
+				$mail->addAddress($user->email, $user->name);
 
-			// 	//Content
-			// 	$mail->isHTML(true);
-			// 	$mail->Subject = 'Kode OTP E-Katalog';
-			// 	$mail->Body = "
-			// 		<div>
-			// 			<h3>Kode Verifikasi Login Company Profile New Armada</h3>
-			// 		</div>
-			// 		<div>
-			// 			<span>Halo $user[name],</span>
-			// 		</div>
-			// 		<div style='padding-top: 10px; padding-bottom: 10px;'>
-			// 			<span>Silahkan masukkan kode OTP ini untuk dapat melanjutkan menggunakan aplikasi New Armada.</span>
-			// 		</div>
-			// 		<div style='background-color: #e3e3e3; padding: 15px;'>
-			// 			<span>OTP</span>
-			// 			<h4>$otp_code</h4>
-			// 			<span>Kode ini berlaku selama 10 menit.<span>
-			// 		</div>
-			// 		<div style='padding-top: 10px; padding-bottom: 10px;'>
-			// 			<span>Jaga keamanan akun Anda dengan tidak membagikan kode OTP kepada siapapun.</span>
-			// 		</div>
-			// 		<span>Terimakasih.</span>
-			// 	";
-			// 	$mail->AltBody = 'OTP Company Profile New Armada = ' . $otp_code;
-            //     if(!$mail->send()){
-            //         return redirect()->route('login')->with('failed', 'Login failed, pleas try again');        
-            //     }else{
-            //         $this->redirectRoute('otp', ['email' => $this->loginForm->email]);
-            //     }            
-			        $this->redirectRoute('otp', ['email' => $this->loginForm->email]);
+				//Content
+				$mail->isHTML(true);
+				$mail->Subject = 'Kode OTP E-Katalog';
+				$mail->Body = "
+					<div>
+						<h3>Kode Verifikasi Login Company Profile New Armada</h3>
+					</div>
+					<div>
+						<span>Halo $user[name],</span>
+					</div>
+					<div style='padding-top: 10px; padding-bottom: 10px;'>
+						<span>Silahkan masukkan kode OTP ini untuk dapat melanjutkan menggunakan aplikasi New Armada.</span>
+					</div>
+					<div style='background-color: #e3e3e3; padding: 15px;'>
+						<span>OTP</span>
+						<h4>$otp_code</h4>
+						<span>Kode ini berlaku selama 10 menit.<span>
+					</div>
+					<div style='padding-top: 10px; padding-bottom: 10px;'>
+						<span>Jaga keamanan akun Anda dengan tidak membagikan kode OTP kepada siapapun.</span>
+					</div>
+					<span>Terimakasih.</span>
+				";
+				$mail->AltBody = 'OTP Company Profile New Armada = ' . $otp_code;
+                if(!$mail->send()){
+                    return redirect()->route('login')->with('failed', 'Login failed, pleas try again');        
+                }else{
+                    $this->redirectRoute('otp', ['email' => $this->loginForm->email]);
+                }            
+			    // $this->redirectRoute('otp', ['email' => $this->loginForm->email]);
 
         }else {
             return redirect()->route('login')->with('failed', 'Login failed, pleas try again');
