d93c89cf53f7a8a4ebeedf0666cf32d4c43fd2ef
Fajar Aji Prayoga <fajaraji.prayoga@gmail.com>
2024-02-05 16:08:53 +0700

    Edit: Change internship into prakerin in navbar

--- resources/views/livewire/navbar/navbar.blade.php
+++ resources/views/livewire/navbar/navbar.blade.php
@@ -104,7 +104,7 @@
                                     <a href="{{route('career')}}" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-400 dark:hover:text-white">Job Portal</a>
                                 </li>
                                 <li>
-                                    <a target="_blank" href="https://prakerin.mekararmadajaya.com" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-400 dark:hover:text-white">Internship</a>
+                                    <a target="_blank" href="https://prakerin.mekararmadajaya.com" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-400 dark:hover:text-white">Prakerin</a>
                                 </li>
                             </ul>
                         </div>
