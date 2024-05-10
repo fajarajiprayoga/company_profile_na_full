2dc7236d3d4eba9b7e71515f8794b182770162cb
fajarajiprayoga <fajaraji.prayoga@gmail.com>
2024-05-10 10:42:59 +0700

    Edit: Remove preloader

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -46,44 +46,9 @@ use Illuminate\Support\Facades\Route;
             body {
                 font-family: 'Poppins', sans-serif;
             }
-            /* CSS for the preloader */
-            .preloader {
-                position: fixed;
-                top: 0;
-                left: 0;
-                width: 100%;
-                height: 100%;
-                background: rgba(255, 255, 255, 0.9);
-                display: flex;
-                justify-content: center;
-                align-items: center;
-                z-index: 9999;
-            }
-
-            .preloader .spinner {
-                border: 8px solid #f3f3f3;
-                border-top: 8px solid #3498db;
-                border-radius: 50%;
-                width: 90px;
-                height: 90px;
-                animation: spin 2s linear infinite;
-            }
-
-            @keyframes spin {
-                0% {
-                    transform: rotate(0deg);
-                }
-                100% {
-                    transform: rotate(360deg);
-                }
-            }
         </style>
     </head>
     <body>
-        <div id="preloader" class="preloader">
-            <div class="spinner"></div>
-        </div>
-
         {{ $slot }}
 
         @livewireScripts
@@ -105,14 +70,6 @@ use Illuminate\Support\Facades\Route;
         <script src="https://cdn.jsdelivr.net/npm/@glidejs/glide"></script>
 
         <script>
-            window.addEventListener('load', function() {
-                var preloader = document.getElementById('preloader');
-                preloader.style.opacity = 0;
-                setTimeout(function() {
-                    preloader.style.display = 'none';
-                }, 500);
-            });
-
         $(document).ready(function(){
 
             $('ul').addClass('list-inside');
