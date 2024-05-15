034407f0f828541d13708a06f05b0937e2014dca
fajarajiprayoga <fajaraji.prayoga@gmail.com>
2024-05-15 11:43:37 +0700

    Edit: path to admin dashboard

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -27,7 +27,7 @@ class AdminPanelProvider extends PanelProvider
         return $panel
             ->default()
             ->id('admin')
-            ->path('admin')
+            ->path('karoseri/admin')
             // ->login()
             ->colors([
                 'primary' => Color::Amber,
