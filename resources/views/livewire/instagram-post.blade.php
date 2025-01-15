472ce0360663a29a528518cc2cca55822694c7f2
fajarajiprayoga <fajaraji.prayoga@gmail.com>
2025-01-15 14:43:04 +0700

    Fix: bug arrow slider

--- resources/views/livewire/instagram-post.blade.php
+++ resources/views/livewire/instagram-post.blade.php
@@ -45,7 +45,7 @@
                     @endforeach
                 </div>
                 <!-- Slider controls -->
-                <button type="button" class="absolute -left-11 top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
+                <button type="button" class="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev style="left: -2.75rem">
                     <span class="inline-flex items-center justify-center w-6 h-6 bg-black">
                         <svg class="w-2 h-2 text-white rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 1 1 5l4 4"/>
@@ -53,7 +53,7 @@
                         <span class="sr-only">Previous</span>
                     </span>
                 </button>
-                <button type="button" class="absolute -right-11 top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next>
+                <button type="button" class="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next style="right: -2.75rem">
                     <span class="inline-flex items-center justify-center w-6 h-6 bg-black">
                         <svg class="w-2 h-2 text-white rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
